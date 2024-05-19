f0506a146a5a1876e685b1ead29ae8660a9cd0e8
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-05-19 21:08:43 +0200

    Feat: complete exercise

---
+++ resources/views/aboutme.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About me</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <header class="bg-primary">
+        <div class="container">
+            <h1>About Me</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  About me
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>I will tell you nothing &#128527;</strong> 
+                </div>
+              </div>
+            </div>
+          </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'name' => 'Mattia',
+        'lastname' => 'Giarrusso',
+        'description'=> 'I am 27 years old, a junior developer studying at Boolean. I am currently taking the full stack web developer course at Boolean; I am also finishing my Master\'s degree in Business Organisation and Marketing at La Sapienza University in Rome.',
+        'age' => 27,
+        'email' => "giar.mat96@gmail.com",
+    ];
+
+    return view('myprofile', $data);
+});
+
+Route::get('/about-me', function() {
+    return view('aboutme');
 });
